91ea296bde4651757b924098a68ede297e70f301
Edward Johnson <edwardcjohnson@users.noreply.github.com>
2016-09-02 18:42:56 -0700

    Create utils.py

---
+++ python/library/utils.py
@@ -0,0 +1,12 @@
+def build_dict(keys, values):
+  """Builds a dictionary from two lists.
+  
+  Args:
+      keys: A list of keys
+      values: A list of values
+      
+  Returns:
+      A dictionary with key-value pairs specified
+      from a list of keys and a list of values.
+  """
+  
