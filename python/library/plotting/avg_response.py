8d40da03369cccd373cd5eb52cd7f7df9da82da6
tjsurf246@aol.com <johnson.edward1990@gmail.com>
2017-02-06 21:48:16 -0800

    formatted lines in plot. Create show option to allow pdf save

--- python/library/plotting/avg_response.py
+++ python/library/plotting/avg_response.py
@@ -23,25 +23,39 @@ def avg_response(df, x, y_obs, y_est, save=False):
         x = pd.cut(df[x], bins=10)
 
     df_grouped = df.groupby([x])[y_obs, y_est].agg({"mean":"mean", "std err":"sem", "count":"count"})
-    x_vals = np.arange(len(df_grouped))
-    ax1.errorbar(x_vals,df_grouped["mean"][y_est],yerr=df_grouped["std err"][y_est], fmt='-',marker='o',color="R",
-             mec='black', ms=5, mew=2)
-    ax1.plot(x_vals, df_grouped["mean"][y_obs], '-', label=y_obs, color = "G", linewidth=2)
-    ax2.bar(x_vals, df_grouped["count"][y_obs], color='DarkSlateGray', alpha = 0.3)
-    ax1.set_xlim(x_vals[0]-1,x_vals[-1]+1)
-    x_levels = list(df_grouped.index)
+    
+    x_vals = range(len(df_grouped))
+    y_vals = df_grouped["mean"][y_est]
+    ax1.errorbar(x_vals, y_vals,yerr=df_grouped["std err"][y_est], fmt='-',
+        marker='o',color="R", mec='black', ms=10, mew=2, linewidth=4, capsize=10, elinewidth=2)
+
+    y_vals = df_grouped["mean"][y_obs]
+    ax1.plot(x_vals, y_vals, '-', label=y_obs, marker='o',
+        color = "G",mec='black', ms=10, mew=2, linewidth=4)
+
+    y_vals = df_grouped["count"][y_obs]
+    ax2.bar(x_vals,y_vals, color='DarkSlateGray', alpha = 0.25)
+
+    ax1.set_xlim(x_vals[0]-0.2,x_vals[-1]+1)
+    x_levels = list(y_vals.index)
     plt.xticks(x_vals, x_levels)
     ax1.set_xticklabels(x_levels, rotation=45)
     ax1.grid(False)
     ax2.grid(False)
-    ax1.set_xlabel(x_name, fontsize=14)
-    ax1.set_ylabel(y_obs, fontsize=14)
-    ax2.set_ylabel("count", fontsize=14)
-    plt.title("Average {y} for groups of {x}".format(x=x_name,y=y_obs),  fontsize=20)
-    ax1.legend([y_obs, y_est])
+    font_size = 20
+    ax1.set_xlabel(x_name, fontsize=font_size)
+    ax1.set_ylabel(y_obs, fontsize=font_size)
+    ax2.set_ylabel("count", fontsize=font_size)
+    plt.title("Average {y} for groups of {x}".format(x=x_name,y=y_obs),  fontsize=font_size+5)
+    ax1.legend([y_obs, y_est], fontsize=font_size-2)
     if save:
         fig.savefig("/home/edward/work/repos/prometheus/python/plots/avg_response/{}.png".format(x_name), bbox_inches='tight')
     plt.show()
+
+# Basic usage and save to png
+avg_response(tips, "day", #"day"
+"tip",
+"total_bill",save=False)
 
 
 # Loop over all variables in df and save to pdf
@@ -52,10 +66,6 @@ for var in tips.dtypes.index:
     pp.savefig()
 pp.close()
 
-# Basic usage and save to png
-avg_response(tips, "tip", #"day"
-"tip",
-"total_bill",save=True)
 
 
 
@@ -63,3 +73,12 @@ avg_response(tips, "tip", #"day"
 sns.boxplot(x="sex", y="total_bill", hue="day", data=tips);plt.show()
 
 sns.pointplot(x="sex", y="total_bill", data=tips);plt.show()
+
+
+
+tip_grouped = tips.groupby(["day"])["tip", "total_bill"].agg({"mean":"mean", "std err":"sem", "count":"count"})
+print(tip_grouped)
+
+print(tip_grouped["mean"]["tip"])
+
+print(tip_grouped["mean"].sort_values(["tip"])["tip"].index)
