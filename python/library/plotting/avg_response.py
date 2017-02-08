7fa5bf4529a14c619eb163682c0c3b6f24704ff2
tjsurf246@aol.com <johnson.edward1990@gmail.com>
2017-02-07 21:23:05 -0800

    created function to build avg_response plot report pdf

--- python/library/plotting/avg_response.py
+++ python/library/plotting/avg_response.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
-import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
 import pandas as pd
 import scipy.stats as stats
 import seaborn as sns
+
 sns.set(style="white",color_codes=True)
 sns.set_palette(sns.cubehelix_palette(8, start=.5, rot=-.75))
 titanic = sns.load_dataset("titanic")
 tips = sns.load_dataset("tips")
 
 
-def avg_response(df, x, y_obs, y_est, save=False):
+def avg_response(df, x, y_obs, y_est, save=False, show=True):
+    """
+    Plots the average estimated response and average observed 
+    response for groups of a specified variable. A bar chart 
+    to display the count within each group is also provided.
+    """
 
     fig, ax1 = plt.subplots(figsize=(15,15))
 
@@ -22,12 +29,14 @@ def avg_response(df, x, y_obs, y_est, save=False):
     elif df[x].dtype == "float":
         x = pd.cut(df[x], bins=10)
 
-    df_grouped = df.groupby([x])[y_obs, y_est].agg({"mean":"mean", "std err":"sem", "count":"count"})
+    metrics = {"mean":"mean", "std err":"sem", "count":"count"}
+    df_grouped = df.groupby([x])[y_obs, y_est].agg(metrics)
     
     x_vals = range(len(df_grouped))
     y_vals = df_grouped["mean"][y_est]
     ax1.errorbar(x_vals, y_vals,yerr=df_grouped["std err"][y_est], fmt='-',
-        marker='o',color="R", mec='black', ms=10, mew=2, linewidth=4, capsize=10, elinewidth=2)
+        marker='o',color="R", mec='black', ms=10, mew=2, linewidth=4, 
+        capsize=10, elinewidth=2)
 
     y_vals = df_grouped["mean"][y_obs]
     ax1.plot(x_vals, y_vals, '-', label=y_obs, marker='o',
@@ -46,26 +55,39 @@ def avg_response(df, x, y_obs, y_est, save=False):
     ax1.set_xlabel(x_name, fontsize=font_size)
     ax1.set_ylabel(y_obs, fontsize=font_size)
     ax2.set_ylabel("count", fontsize=font_size)
-    plt.title("Average {y} for groups of {x}".format(x=x_name,y=y_obs),  fontsize=font_size+5)
+    plt.title("Average {y} for groups of {x}".format(x=x_name, y=y_obs), 
+        fontsize=font_size+5)
     ax1.legend([y_obs, y_est], fontsize=font_size-2)
     if save:
-        fig.savefig("/home/edward/work/repos/prometheus/python/plots/avg_response/{}.png".format(x_name), bbox_inches='tight')
-    plt.show()
+        fig.savefig("/home/edward/work/repos/prometheus/python/plots/avg_response/{}.png".
+            format(x_name), bbox_inches='tight')
+    if show:
+        plt.show()
 
-# Basic usage and save to png
+
+
+def avg_response_report(df, var_list, y_obs, y_est, file):
+    """
+    Creates a pdf report with avg response plots for each
+    variable specified in var_list.
+    """
+    page = PdfPages(file)
+    for var in var_list:
+        avg_response(df, var, y_obs, y_est, show=False)
+        page.savefig()
+    page.close()
+
+
+
+# ------------------------------------
+# Basic usage 
+# ------------------------------------
 avg_response(tips, "day", #"day"
 "tip",
 "total_bill",save=False)
 
-
-# Loop over all variables in df and save to pdf
-from matplotlib.backends.backend_pdf import PdfPages
-pp = PdfPages('/home/edward/work/repos/prometheus/python/plots/avg_response/multipage.pdf')
-for var in tips.dtypes.index:
-    avg_response(tips, var, "tip", "total_bill")
-    pp.savefig()
-pp.close()
-
+avg_response_report(tips, tips.dtypes.index, "tip", "total_bill",
+    '/home/edward/work/repos/prometheus/python/plots/avg_response/multipage.pdf')
 
 
 
@@ -73,12 +95,3 @@ pp.close()
 sns.boxplot(x="sex", y="total_bill", hue="day", data=tips);plt.show()
 
 sns.pointplot(x="sex", y="total_bill", data=tips);plt.show()
-
-
-
-tip_grouped = tips.groupby(["day"])["tip", "total_bill"].agg({"mean":"mean", "std err":"sem", "count":"count"})
-print(tip_grouped)
-
-print(tip_grouped["mean"]["tip"])
-
-print(tip_grouped["mean"].sort_values(["tip"])["tip"].index)
