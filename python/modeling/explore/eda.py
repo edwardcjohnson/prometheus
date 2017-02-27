59a09a0183f6e587f89005b75da3204b546238e0
Edward Johnson <edwardcjohnson@users.noreply.github.com>
2017-02-27 09:17:52 -0800

    Update and rename eda_plots.py to eda.py

--- python/modeling/explore/eda.py
+++ python/modeling/explore/eda.py
@@ -12,6 +12,23 @@ df = pd.read_csv(file, sep=', ')
 
 df.head(5) # print first 5 rows
 df.describe() # get summary of numeric values
+df.dtypes # print data types of cols
+
+df.groupby(['class']).describe() # get summary of levels within a cat var
+
+#-----------------------------------------
+# Type Casting
+#-----------------------------------------
+df['date1'] = pd.to_datetime(df['date1'])
+df[['col2','col3']] = df[['col2','col3']].apply(pd.to_numeric)
+
+#-----------------------------------------
+# Transformations
+#-----------------------------------------
+(df['date1'] - df['date2']).astype('timedelta64[h]') # num of hours between dates
+df['hour'] = df['date1'].hour
+df['day'] = df['date1].weekday
+df['day_of_year'] = df['date1].dayofyear
 
 #-----------------------------------------
 # Exploratory Plots
@@ -26,10 +43,10 @@ plt.figure()
 df.diff().hist(color='k', alpha=0.5, bins=50)
 
 # Plot all vars grouped by the response var "class"
-df.groupby('class').hist()
+df.groupby(['class']).hist()
 
 # Plot the distn of variable "var1" by the response 
-data.groupby('class').var1.hist(alpha=0.4)
+data.groupby(['class']).var1.hist(alpha=0.4)
 
 # plot boxplots for each variable
 df.boxplot()
