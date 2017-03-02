3cb33b43168b258d61e49cff491f897f2feb4349
Edward Johnson <edwardcjohnson@users.noreply.github.com>
2017-03-01 17:13:03 -0800

    Update helpers.py

--- python/library/utilities/helpers.py
+++ python/library/utilities/helpers.py
@@ -26,34 +26,3 @@ def banner(text, ch='=', length=78):
     spaced_text = ' %s ' % text
     banner = spaced_text.center(length, ch)
     return banner
-  
-def check_kerberos_ticket():
-    """check if user has a valid kerberos ticket
-    registered.
-
-    Returns:
-            A message indicating a kerberos ticket
-            was found or not. If a ticket is not found
-            then the program will exit and a message
-            will be printed to stderr.
-    """
-
-    if subprocess.call(['klist', '-s']) == 0:
-        print "\nValid kerberos ticket found\n"
-    else:
-        sys.exit("Error: No kerberos ticket found")
-  
-def exe_impala_script(impala_server, sql_file):
-    """Executes an Impala query specified in
-    sql_file.
-
-    Args:
-        impala_server: a string specifying the impala server
-                       e.g. "hrtimpslb.allstate.com"
-        sql_file: the sql file containing the query you want
-                  executed via impala. 
-
-    """
-    os.system("""
-        impala-shell -ki  {impala_conn}  --query_file={file}
-        """.format(file = sql_file, impala_conn = impala_server))
